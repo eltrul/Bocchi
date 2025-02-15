56fd5b83caa22152a381a859c2c594c76661f51c
eltrul <105537798+eltrul@users.noreply.github.com>
2025-02-15 18:14:33 +0700

    Create BFKaitunConstants.lua

---
+++ BFKaitunConstants.lua
@@ -0,0 +1,28 @@
+
+MeleesTable = {
+    "Black Leg",
+    "Electro",
+    "Fishman Karate",
+    "Dragon Claw",
+    "Superhuman",
+    "Death Step",
+    "Electric Claw",
+    "Sharkman Karate",
+    "Dragon Talon",
+    "Godhuman",
+    "SanguineArt"
+}
+
+MeleesId =  {
+    "BlackLeg",
+    "Electro",
+    "FishmanKarate",
+    "DragonClaw",
+    "Superhuman",
+    "DeathStep",
+    "ElectricClaw",
+    "SharkmanKarate",
+    "DragonTalon",
+    "Godhuman",
+    "SanguineArt"
+} 
